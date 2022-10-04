c5432c924fee1318dbf2ffb823f7d338c9c61be3
ZhouSusan <szhou089@gmail.com>
2022-10-04 10:37:13 -0700

    added try and catch exception

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 
 class AddNumbers {
     public static void main(String[] args) {
-        int a, b, c;
-        System.out.println("Please enter two integers:");
+        try {
+            int a, b, c;
+            System.out.println("Please enter two integers:");
 
-        Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);
 
-        a = input.nextInt();
-        b = input.nextInt();
-        c = a + b;
+            a = input.nextInt();
+            b = input.nextInt();
+            c = a + b;
 
-        System.out.println("The sum of " + a + " + " + b + " = " + c);
+            System.out.println("The sum of " + a + " + " + b + " = " + c);
+        } catch( Exception e) {
+            System.out.println("Sorry, this is not an integer. Please try again and enter two integers.");
+        }
     }
 }
